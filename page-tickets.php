f77f75d3adf1d2ba1012b1657bf805508bc62819
branddevelopers1 <info@branddevelopers.ca>
2026-05-26 13:18:18 -0400

    update pricing text

--- page-tickets.php
+++ page-tickets.php
@@ -13,8 +13,8 @@ $table_ticket_url = fest_setting('fest_table_ticket_url') ?: 'https://buy.tablel
 
 $prices = [
     'day1' => [
-        'ga'    => fest_setting('fest_day1_ga_price') ?: 'Starts at $40',
-        'vip'   => fest_setting('fest_day1_vip_price') ?: 'Starts at $80',
+        'ga'    => (fest_setting('fest_day1_ga_price')  ?: 'TBA') !== 'TBA' ? fest_setting('fest_day1_ga_price')  : 'Starts at $40',
+        'vip'   => (fest_setting('fest_day1_vip_price') ?: 'TBA') !== 'TBA' ? fest_setting('fest_day1_vip_price') : 'Starts at $80',
         'table' => fest_setting('fest_day1_table_price') ?: 'TBA',
     ],
     'day2' => [
